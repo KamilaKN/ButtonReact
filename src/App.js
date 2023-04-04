e75e0faa5ee60f84a850d8790d7f9b06840296f6
Kamila KN <rysbekovakamila00@gmail.com>
2023-04-04 14:41:59 +0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counterLeft, setCounterLeft] = useState(0)
+  const [counterRight, setCounterRight] = useState(0)
+  const [counterHistory, setCounterHistory] = useState('')
+  // setTimeout(() => {
+  //   setCounter(counter + 1)
+  // }, 2000)
+
+  const leftClick = () => {
+    setCounterLeft(counterLeft + 1)
+    setCounterHistory(counterHistory + 'L')
+  }
+  const rightClick = () => {
+    setCounterRight(counterRight + 1)
+    setCounterHistory(counterHistory + 'R' )
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p style={{fontSize: '60px'}}>Клик левой кнопкой {counterLeft} </p>
+      <p style={{fontSize: '60px'}}>Клик правой кнопкой {counterRight} </p>
+      <button onClick={leftClick}>Клик левой</button>
+      <button onClick={rightClick}>Клик правой</button>
+
+      <h1>История кликов{counterHistory}</h1>
     </div>
   );
 }
